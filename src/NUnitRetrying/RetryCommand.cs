dd058cc76b6120a9da3074135869b4607ab0f19d
Henrry Salinas <es.henrry@gmail.com>
2019-01-25 21:00:13 -0400

    updating retry commando

---
+++ src/NUnitRetrying/RetryCommand.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitRetrying
+{
+    /// <summary>
+    /// Retry Test Command decorator.
+    /// </summary>
+    public class RetryingCommand : DelegatingTestCommand
+    {
+        /// <summary>
+        /// Times to retry.
+        /// </summary>
+        private readonly int _times;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="RetryingCommand"/>.
+        /// </summary>
+        /// <param name="innerCommand"></param>
+        /// <param name="times"></param>
+        public RetryingCommand(TestCommand innerCommand, int times)
+            : base(innerCommand)
+        {
+            _times = times;
+        }
+
+        /// <summary>
+        /// Executes the test.
+        /// </summary>
+        /// <param name="context">Current Test context.</param>
+        /// <returns>The Test results.</returns>
+        public override TestResult Execute(TestExecutionContext context)
+        {
+            int count = _times;
+
+            while (count-- > 0)
+            {
+                try
+                {
+                    RunTest(context);
+                }
+                catch (Exception ex)
+                {
+                    if (context.CurrentResult == null)
+                    {
+                        context.CurrentResult = context.CurrentTest.MakeTestResult();
+                    }
+                    context.CurrentResult.RecordException(ex);
+                }
+                context.CurrentRepeatCount++;
+                if (!TestFailed(context))
+                {
+                    break;
+                }
+                if (count > 0)
+                {
+                    context.CurrentResult = context.CurrentTest.MakeTestResult();
+                }
+
+                if (context.CurrentRepeatCount <= _times)
+                {
+                    context.OutWriter.WriteLine();
+                    context.OutWriter.WriteLine($"Test retried {context.CurrentRepeatCount} time/s.");
+                }
+            }
+
+            return context.CurrentResult;
+        }
+
+        /// <summary>
+        /// Executes the given Test in the given context.s
+        /// </summary>
+        /// <param name="context">Current Test Context.</param>
+        private void RunTest(TestExecutionContext context)
+        {
+            context.CurrentResult = innerCommand.Execute(context);
+        }
+
+        /// <summary>
+        /// Check if the Test is failed.
+        /// </summary>
+        /// <param name="context">Current Test Context.</param>
+        /// <returns>true if the test has failed otherwise false.</returns>
+        private static bool TestFailed(TestExecutionContext context)
+        {
+            return UnsuccessfulResultStates.Contains(context.CurrentResult.ResultState);
+        }
+
+        /// <summary>
+        /// Possible states of a failed Test.
+        /// </summary>
+        private static ResultState[] UnsuccessfulResultStates => new[]
+        {
+                ResultState.Failure,
+                ResultState.Error,
+                ResultState.Cancelled,
+                ResultState.SetUpError,
+                ResultState.SetUpFailure,
+        };
+    }
+}
--- src/NUnitRetrying/RetryingAttribute.cs
+++ src/NUnitRetrying/RetryingAttribute.cs
@@ -5,6 +5,7 @@
     using NUnit.Framework.Interfaces;
     using NUnit.Framework.Internal;
     using NUnit.Framework.Internal.Commands;
+    using NUnitRetrying;
     using NUnitRetrying.Environment;
 
     /// <summary>
@@ -44,102 +45,6 @@
         public TestCommand Wrap(TestCommand command)
         {
             return new RetryingCommand(command, Times);
-        }
-
-        /// <summary>
-        /// Retry Test Command decorator.
-        /// </summary>
-        private class RetryingCommand : DelegatingTestCommand
-        {
-            /// <summary>
-            /// Times to retry.
-            /// </summary>
-            private readonly int _times;
-
-            /// <summary>
-            /// Initializes a new instance of <see cref="RetryingCommand"/>.
-            /// </summary>
-            /// <param name="innerCommand"></param>
-            /// <param name="times"></param>
-            public RetryingCommand(TestCommand innerCommand, int times)
-                : base(innerCommand)
-            {
-                _times = times;
-            }
-
-            /// <summary>
-            /// Executes the test.
-            /// </summary>
-            /// <param name="context">Current Test context.</param>
-            /// <returns>The Test results.</returns>
-            public override TestResult Execute(TestExecutionContext context)
-            {
-                int count = _times;
-
-                while (count-- > 0)
-                {
-                    try
-                    {
-                        RunTest(context);
-                    }
-                    catch (Exception ex)
-                    {
-                        if (context.CurrentResult == null)
-                        {
-                            context.CurrentResult = context.CurrentTest.MakeTestResult();
-                        }
-                        context.CurrentResult.RecordException(ex);
-                    }
-                    context.CurrentRepeatCount++; // increment Retry count for next iteration. will only happen if we are guaranteed another iteration
-                    if (!TestFailed(context))
-                    {
-                        break;
-                    }
-                    if (count > 0)
-                    {
-                        context.CurrentResult = context.CurrentTest.MakeTestResult();
-                    }
-
-                    if (context.CurrentRepeatCount <= _times)
-                    {
-                        context.OutWriter.WriteLine();
-                        context.OutWriter.WriteLine($"Test retried {context.CurrentRepeatCount} time/s.");
-                    }
-                }
-
-                return context.CurrentResult;
-            }
-
-            /// <summary>
-            /// Executes the given Test in the given context.s
-            /// </summary>
-            /// <param name="context">Current Test Context.</param>
-            private void RunTest(TestExecutionContext context)
-            {
-                context.CurrentResult = innerCommand.Execute(context);
-            }
-
-            /// <summary>
-            /// Check if the Test is failed.
-            /// </summary>
-            /// <param name="context">Current Test Context.</param>
-            /// <returns>true if the test has failed otherwise false.</returns>
-            private static bool TestFailed(TestExecutionContext context)
-            {
-                return UnsuccessfulResultStates.Contains(context.CurrentResult.ResultState);
-            }
-
-            /// <summary>
-            /// Possible states of a failed Test.
-            /// </summary>
-            private static ResultState[] UnsuccessfulResultStates => new[]
-            {
-                ResultState.Failure,
-                ResultState.Error,
-                ResultState.Cancelled,
-                ResultState.SetUpError,
-                ResultState.SetUpFailure,
-            };
         }
     }
 }
